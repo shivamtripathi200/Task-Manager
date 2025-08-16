89e5443e9dfb00e8da794e4c4ba3d42e343d8f56
Shivam Tripathi <tripathishubham095@gmail.com>
2025-08-16 21:09:59 +0530

    debugging add feature

--- taskmanager.py
+++ taskmanager.py
@@ -26,18 +26,24 @@ class Task:
             "created at": self.created_at
         }
     
-def load_data():
-    with open(DB_FILE,"r") as f:
-        return json.load(f)
 def save_data(data:dict):
     with open(DB_FILE,"w") as f:
         json.dump(data, f, indent = 4)
 
+def load_data():
+    if not DB_FILE.exists():
+        initialdata = {"tasks":[],"next_id":1}
+        save_data(initialdata)
+        return initialdata
+    with open(DB_FILE,"r") as f:
+        return json.load(f)
+
+
 @app.command(help="function to insert a task")
-def add(
+def edit(
     title: str = typer.Argument(),
-    priority: str = typer.Option(),
-    due:Optional[str] = typer.Option()
+    priority: str = typer.Option("medium"),
+    due:Optional[str] = typer.Option(None)
 ):
     T = Task(id = 1, title = title, priority = priority, due = due)
     save_data(T.to_dict())
@@ -47,9 +53,22 @@ def add(
 def list():
     print(json.dumps(load_data(), indent=4))
 
-
 @app.command()
-def edit(title: Optional[str], priority: Optional[str], status: Optional[str],due: Optional[str]):
+def add(
+    title: str = typer.Argument(),
+    priority: str = typer.Option("medium"),
+    due:Optional[str] = typer.Option(None)
+):
+    data = load_data()
+    data_base_task = data["tasks"]
+    next_id = data.get("next_id",1)
+    next_id +=1
+    temp = Task(id = next_id , title= title,priority=priority,due=due)
+    data_base_task.append(temp.to_dict())
+    save_data(data)
+
+
+
     
 
 if __name__ == "__main__":
